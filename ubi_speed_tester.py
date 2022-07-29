d4e96d3826497b52f79b797d88d0c903edc12475
Jinesh Soni <hi@jineshsoni.com>
2022-07-29 15:02:51 +0530

    fixex issue

--- ubi_speed_tester.py
+++ ubi_speed_tester.py
@@ -18,15 +18,16 @@ temp = check_CPU_temp()
 try:
  st = speedtest.Speedtest()
  st.get_best_server()
+ 
  upload = round(st.upload() / 1000000, 2)
  download = round(st.download() / 1000000, 2)
  ping = round(st.results.ping, 2)
 
  payload = {'Download': download, 'Upload': upload, 'Ping': ping, 'Temp': temp}
- #message = 'Download: ' + download + '\n' + 'Uplaod: ' + uplaod + 'n' + 'Ping: ' + ping + '\n' + 'CPU: ' + str(temp)
- 
+ message = 'Download: ' + str(download) + '\n' + 'Uplaod: ' + str(upload) + '\n' + 'Ping: ' + str(ping) + '\n' + 'CPU: ' + str(temp)
+
  r = requests.post('http://industrial.api.ubidots.com/api/v1.6/devices/raspberry-pi/?token='+ubiToken, data=payload)
- r = requests.get('https://api.telegram.org/bot'+telegramBot+'/sendMessage?chat_id='+chatId+'&text='+str(payload))
+ r = requests.get('https://api.telegram.org/bot'+telegramBot+'/sendMessage?chat_id='+chatId+'&text='+str(message))
  
  # Print the server's response Uncomment the next line for debugging purposes
  #print(r.content)
